af0dfdf43600f0e91558315093aac2226cadaaa0
Mokeppello <159796754+Mokeppello@users.noreply.github.com>
2024-11-17 13:43:11 +0500

    1
    1

---
+++ src/main/java/com/mkp/zoo/entity/Animal.java
@@ -0,0 +1,22 @@
+package com.mkp.zoo.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
+
+@Data
+@Entity
+public class Animal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String type;
+    @Column(nullable = false)
+    @Min(0)
+    @Max(100)
+    private int age;
+}
---
+++ src/main/java/com/mkp/zoo/repository/AnimalRepository.java
@@ -0,0 +1,9 @@
+package com.mkp.zoo.repository;
+
+import com.mkp.zoo.entity.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+}
